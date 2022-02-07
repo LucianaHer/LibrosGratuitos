34ed4d9318a92b38b61cceef2f9e80fadbea96eb
Luciana Hernandez <lucianaher74@gmail.com>
2022-02-07 17:05:49 -0300

    responsive details

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -13,57 +13,57 @@ function BookDetail({ book }) {
   }
 
   return (
-      <div className="all">
-        <img className="imageDetail" src={book.cover} alt="" />
+    <div className="all">
+      <img className="imageDetail" src={book.cover} alt="" />
 
-        <div className="rightSide">
-          <div className="data">
-            <div className="titleBook">
-              <h2>{book.title}</h2>
-            </div>
-            <div className="autorBook">
-              <h3>Autor/a: {book.author}</h3>
-            </div>
-            <div className="detalles">
-              <h3>Detalles</h3>
-            </div>
-            <div>
-              <span className="infoTitle"> Año:</span>{" "}
-              <span>{book.publisher_date}</span>
-            </div>
-            <div>
-              <span className="infoTitle">Editor:</span>{" "}
-              <span>{book.publisher}</span>
-            </div>
-            <div>
-              <span className="infoTitle">Páginas:</span>{" "}
-              <span>{book.pages} págs.</span>
-            </div>
-            <div className="detalles">
-              <h3>Contenido</h3>
-            </div>
-            <div className="contentBook">{content}</div>
-            <div className="detalles">
-              <h3>Categorías</h3>
-            </div>
-            <div className="categContainer">
-              {book.categories.map((c) => (
-                <div key={c.category_id} className="categ">
-                  {c.name}
-                </div>
-              ))}
-            </div>
-            <div className="link">
-              <a className="link" href={book.url_download} target="">
-                Enlace a Open Libra para abrir/descargar PDF
-              </a>
-            </div>
+      <div className="rightSide">
+        <div className="data">
+          <div className="titleBook">
+            <h2>{book.title}</h2>
+          </div>
+          <div className="autorBook">
+            <h3>Autor/a: {book.author}</h3>
+          </div>
+          <div className="detalles">
+            <h3>Detalles</h3>
+          </div>
+          <div>
+            <span className="infoTitle"> Año:</span>{" "}
+            <span>{book.publisher_date}</span>
+          </div>
+          <div>
+            <span className="infoTitle">Editor:</span>{" "}
+            <span>{book.publisher}</span>
+          </div>
+          <div>
+            <span className="infoTitle">Páginas:</span>{" "}
+            <span>{book.pages} págs.</span>
+          </div>
+          <div className="detalles">
+            <h3>Contenido</h3>
+          </div>
+          <div className="contentBook">{content}</div>
+          <div className="detalles">
+            <h3>Categorías</h3>
+          </div>
+          <div className="categContainer">
+            {book.categories.map((c) => (
+              <div key={c.category_id} className="categ">
+                {c.name}
+              </div>
+            ))}
+          </div>
+          <div className="link">
+            <a className="link" href={book.url_download} target="">
+              Enlace a Open Libra para abrir/descargar PDF
+            </a>
           </div>
           <button className="volver" onClick={volver}>
             VOLVER
           </button>
         </div>
       </div>
+    </div>
   );
 }
 
